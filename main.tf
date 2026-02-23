761ee57148e0cec87624bfca0f1cf3226b4ce1ff
Manish Suresh Ranglani <ranglani.manish90@gmail.com>
2026-02-23 15:02:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,7 @@ module "key-vault" {
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   public_network_access_enabled = lookup(each.value, "public_network_access_enabled", false)
   network_acls_default_action   = lookup(each.value, "network_acls_default_action", "Deny")
-  network_acls_bypass           = lookup(each.value, "network_acls_bypass", "AzureServices")
-  purge_protection_enabled      = lookup(each.value, "purge_protection_enabled", true)
-  enable_rbac_authorization     = lookup(each.value, "enable_rbac_authorization", true)
-  soft_delete_retention_days    = lookup(each.value, "soft_delete_retention_days", 90)
+  soft_delete_retention_days    = lookup(each.value, "soft_delete_retention_days", 60)
   sku_name                      = lookup(each.value, "sku_name", "standard")
   AWS_ACCESS_KEY_ID = "AKIAQJKDGTJWRPO87GH"     #This is dummy Access Key
 }
